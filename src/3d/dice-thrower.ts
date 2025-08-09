e5bcda16e816a052e813f6bd43d532c882cb8f16
Darragh Van Tichelen <info@darragh.dev>
2025-08-09 14:56:39 +0200

    Improve 3d raycast check

--- src/3d/dice-thrower.ts
+++ src/3d/dice-thrower.ts
@@ -144,7 +144,7 @@ export class DiceThrower {
                     activeRoll.done = true;
 
                     const ray = new Ray(activeRoll.mesh.position, activeRoll.pickVector ?? new Vector3(0, 1, 0), 100);
-                    const pickResult = this.scene.pickWithRay(ray);
+                    const pickResult = this.scene.pickWithRay(ray, (mesh) => mesh === activeRoll.mesh);
                     if (pickResult?.hit) {
                         activeRoll.resolve({ dieName: activeRoll.dieName, faceId: pickResult.faceId });
                     } else {
